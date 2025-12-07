a900cd91bc73e555aa934bda2016674974190a1d
Ofek Itzhaki <77631987+OfekItzhaki@users.noreply.github.com>
2025-12-07 21:01:04 +0200

    Added logic for reminders

--- todo-backend/src/app.module.ts
+++ todo-backend/src/app.module.ts
@@ -1,35 +1,29 @@
 import { Module } from '@nestjs/common';
 import AppService from './app.service';
 import AppController from './app.controller';
-import { TodoListsController } from './todo-lists/todo-lists.controller';
-import { TodoListsService } from './todo-lists/todo-lists.service';
-import { TasksController } from './tasks/tasks.controller';
-import { TasksService } from './tasks/tasks.service';
-import { ListSharesController } from './list-shares/list-shares.controller';
-import { ListSharesService } from './list-shares/list-shares.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { StepsController } from './steps/steps.controller';
-import { StepsService } from './steps/steps.service';
-import { MeController } from './me/me.controller';
+import { TodoListsModule } from './todo-lists/todo-lists.module';
+import { TasksModule } from './tasks/tasks.module';
+import { StepsModule } from './steps/steps.module';
+import { ListSharesModule } from './list-shares/list-shares.module';
+import { MeModule } from './me/me.module';
+import { RemindersModule } from './reminders/reminders.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
-  controllers: [
-    AppController,
-    TodoListsController,
-    TasksController,
-    ListSharesController,
-    StepsController,
-    MeController,
+  imports: [
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    TodoListsModule,
+    TasksModule,
+    StepsModule,
+    ListSharesModule,
+    MeModule,
+    RemindersModule,
   ],
-  providers: [
-    AppService,
-    TodoListsService,
-    TasksService,
-    ListSharesService,
-    StepsService,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
